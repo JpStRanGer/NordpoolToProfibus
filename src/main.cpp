9bf17e5e6e47fdf5a756028ce7adfbf1aa01c97a
jonasnic <nicolaysen89@gmail.com>
2022-08-23 09:47:10 +0200

    added changes to main to use NP

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
 #include <Arduino.h>
 #include <Ethernet.h>
 #include "func.h"
-
-void startSerial();
-void startEthernet(EthernetClient client, char server[]);
-bool checkHTTPstatus(EthernetClient client);
-bool SkipHTTPheaders(EthernetClient client);
-void json(EthernetClient client);
+#include "NordPool.h"
 
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 // IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "nordpoolveas.jonaspettersen.no"; // name address for Google (using DNS)
+String server = "nordpoolveas.jonaspettersen.no"; // name address for Google (using DNS)
 // char server[] = "www.google.com";    // name address for Google (using DNS)
 
 // Initialize the Ethernet client library
@@ -19,27 +14,25 @@ char server[] = "nordpoolveas.jonaspettersen.no"; // name address for Google (us
 // that you want to connect to (port 80 is default for HTTP):
 // EthernetClient client;
 
+EthernetClient client;
+NordPool NP;
+
 void setup()
 {
     // Open serial communications and wait for port to open:
     startSerial();
-    startEthernet(server);
-
-    // checkHTTPstatus()?Serial.println("##HTTP status OK!##"):Serial.println("##HTTP status FAILD!##");
-    SkipHTTPheaders() ? Serial.println("## Skip HTTP headers OK! ##") : Serial.println("## Skip HTTP headers FAILD! ##");
-
-    Serial.println("## Start json() ##");
-    json(); //?Serial.println("##HTTP status OK!##"):Serial.println("##HTTP status FAILD!##");;
-    Serial.println("## End json() ##");
-
-    EthernetClient client;
-    Serial.print("\n\nclient.connected():\n");
-    int abc;
-    Serial.print(abc);
+    startEthernet();
+    NP.setServer(server);
 }
 
 void loop()
 {
+    static int i =0;
+    Serial.println(i++);
+
+    NP.update();
+    NP.print();
+    delay(15000);
     ////////////////////
     ////// DEBUG //////
     //////////////////
