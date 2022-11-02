1df61bfd301aa36594378e6aeec97642861337d0
JpStRanGer <jonas.st.pettersen@gmail.com>
2022-11-02 10:56:10 +0100

    Feature create rest reader (#7)
    * FIX(Debugger.hpp): better function in start serial
    
    * DOC(all): added docblocks to the start off all files
    
    * DEV(RESTReader): added Methods -> CheckHTTOstatus(), startSerial()
    
    * wip
    
    * WIP(RESTReader): Made Debug method
    
    * Bug(RestReader): debug method works after pointer as parameter

--- lib/RESTReader/RESTReader.h
+++ lib/RESTReader/RESTReader.h
@@ -1,6 +1,6 @@
 /**
  * @file RESTReader.h
- * @author your name (you@domain.com)
+ * @author  Jonas Pettersen (Jonas.st.pettersen@gmail.com)
  * @brief
  * @version 0.1
  * @date 2022-10-28
@@ -12,16 +12,29 @@
 #define RESTReader_h
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
 class RESTReader
 {
 private:
-/// @brief PRIVATE PROPERTIES/METHODS
+    /// @brief PRIVATE PROPERTIES/METHODS
 public:
-/// @brief PUBLIC PROPERTIES/METHODS
+    /// @brief PUBLIC PROPERTIES
+    bool shouldDebug = true;
+    EthernetClient client;
+    char server[]; // name address for Google (using DNS)
+
+    /// @brief PUBLIC METHODS
     RESTReader(); /// @brief Construct a new RESTReader object
     ~RESTReader(); /// @brief Destroy the RESTReader object
     void test();
+    bool checkHTTPstatus();
+    bool SkipHTTPheaders();
+    void startSerial();
+    void printf(char *fmt, ... ); // Creating a printf() wrapper
+    void debug(char *msg);
 };
 
 #endif
