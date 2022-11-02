79ee7ca8189882f51a547a3008a592748e705c5a
JpStRanGer <jonas.st.pettersen@gmail.com>
2022-11-02 13:57:43 +0100

    Refactoring restreader debug method (#9)
    * FIX(Debugger.hpp): better function in start serial
    
    * DOC(all): added docblocks to the start off all files
    
    * DEV(RESTReader): added Methods -> CheckHTTOstatus(), startSerial()
    
    * wip
    
    * WIP(RESTReader): Made Debug method
    
    * Bug(RestReader): debug method works after pointer as parameter
    
    * DEV(RESTReader): RESTreader worls

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
+/**
+ * @file main.cpp
+ * @author  Jonas Pettersen (Jonas.st.pettersen@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-01
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <Arduino.h>
-#include <Ethernet.h>
 #include <Debugger.hpp>
 #include <RESTReader.h>
 
-/// @brief 
-//Debugger debugger();
+/// @brief
+Debugger debugger;
+RESTReader restReader;
+
 
 void setup()
 {
-  //  debugger.startSerial();
+    debugger.startSerial();
+
+    restReader.test();
+    restReader.checkHTTPstatus();
+    //restReader.SkipHTTPheaders();
 }
 
 void loop()
 {
+    restReader.DEBUG_printOneLineFromHTTP();
+    Serial.print("\n");delay(1000);
 }
