9243914ea88a70068a4ddd86f2c2e9afaa7d739d
Jonas Pettersen <jonas.st.pettersen@gmail.com>
2022-11-25 12:46:13 +0100

    feat(HttpClient): Created HttpClient Class

---
+++ lib/HttpClient/HttpClient.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file HttpClient.cpp
+ * @author Jonas Pettersen (Jonas.st.pettersen@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "HttpClient.h"
---
+++ lib/HttpClient/HttpClient.h
@@ -0,0 +1,25 @@
+/**
+ * @file HttpClient.h
+ * @author Jonas Pettersen (Jonas.st.pettersen@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef HttpClient_h
+#define HttpClient_h
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+//#include <SPI.h>
+#include <Ethernet.h>
+#include <structs.h>
+
+class HttpClient
+{
+
+}
+
+#endif
